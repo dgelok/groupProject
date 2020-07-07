2ac1e36faa112e472a010f25c615e1b67e035829
Dan Gelok <dgelok@gmail.com>
2020-07-07 15:53:39 -0500

    news api testing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// main script file
+$(()=>{
+    var term = "nokia";
+    var url = `https://newsapi.org/v2/everything?q=${term}&apiKey=820d9373d7ea4f3e9d015527ab07b473`
+    
+    $.get(url)
+    .done(function (response) {
+        // console.log(response.articles)
+        for (let i = 0; i < response.articles.length; i++) {
+            console.log(response.articles[i].title)
+        }
+
+        
+    })
+})
