70430ee66dfae3ee28fe64aa22c690953ae26887
Dan Gelok <dgelok@gmail.com>
2020-07-07 19:48:55 -0500

    updated firebase authentication

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDfbgvNY5XxEmdDk3KcOI-w0MEBMhMHm-Y",
+    authDomain: "stock-market-playground.firebaseapp.com",
+    databaseURL: "https://stock-market-playground.firebaseio.com",
+    projectId: "stock-market-playground",
+    storageBucket: "stock-market-playground.appspot.com",
+    messagingSenderId: "541187275911",
+    appId: "1:541187275911:web:e9f65bf84216f95ed28c22",
+    measurementId: "G-PVE2WQ9NZJ"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(()=>{
     var term = "nokia";
-    var url = `https://newsapi.org/v2/everything?q=${term}&apiKey=820d9373d7ea4f3e9d015527ab07b473`
+    var url = `https://newsapi.org/v2/everything?q=${term}&sortBy=relevancy&apiKey=820d9373d7ea4f3e9d015527ab07b473`
     
     $.get(url)
     .done(function (response) {
         // console.log(response.articles)
         for (let i = 0; i < response.articles.length; i++) {
-            console.log(response.articles[i].title)
+            // console.log(response.articles[i].title)
         }
 
         
