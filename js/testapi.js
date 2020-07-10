c430dd3469b17cea1ae8ad8a55c1ed1d9b9da20e
Cainan Barboza <CainanBarboza1@gmail.com>
2020-07-10 13:40:24 -0500

    minor changes to refresh button

--- js/testapi.js
+++ js/testapi.js
@@ -69,6 +69,7 @@ class Holding {
         this.symbol = symbol;
         this.totalShares = totalShares;
         }
+
     
 }
 
@@ -92,12 +93,23 @@ class User{
         let newHolding = new Holding(name,symbol,numShares)
        this.holdings.push(newHolding)
     }
+    buyStock(name, symbol, numShares, latestPrice){
+        if(this.cash >= numShares * latestPrice){
+            this.createNewHolding(name, symbol, numShares)
+        }
+        
+    }
+    async getStockLatestPrice(stockSymbol){
+        let response = await fetch(`https://cloud.iexapis.com/stable/stock/${stockSymbol}/quote/?token=${APIurls[2]}`)
+            let json = await response.json();
+            let currentPrice = json.latestPrice;
+            console.log(currentPrice);
+            return currentPrice;
+    }
     async getNetWorth(){
         let currentTotal = 0;
         for(let comp of this.holdings){
-            let response = await fetch(`https://cloud.iexapis.com/stable/stock/${comp.symbol}/quote/?token=${APIurls[2]}`)
-            let json = await response.json();
-            let currentPrice = json.latestPrice;
+            
             let currentShareTotal = currentPrice * comp.totalShares
             console.log(`Total current value for ${comp.name} with 
             ${comp.totalShares} is ${currentShareTotal}
@@ -194,14 +206,16 @@ let currentUser = createNewUser(localStorage.currentUser);
 //currentUser.clearChart()
 currentUser.getData()
 
-//currentUser.createNewHolding("Microsoft","MSFT", 6)
-//currentUser.saveUser()
+// currentUser.createNewHolding("Microsoft","MSFT", 6)
+// currentUser.saveUser()
 $("#currentUserCont").html(`${currentUser.userName}`)
 console.log(currentUser.userName);
 
  $("#refreshButton").click(function(e){
     currentUser.getData();
  })
+
+//  currentUser.getStockLatestPrice("MSFT")
 //   $("#nameList").click(function(e){
 //       console.log(e.target.id)
 //       let stockSymbol = e.target.id
