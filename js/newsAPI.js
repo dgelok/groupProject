68976bcd8eec516148f43b4e81ada73e72537c2c
Dan Gelok <dgelok@gmail.com>
2020-07-11 16:27:18 -0500

    connecting newsAPI to checkout click function

--- js/newsAPI.js
+++ js/newsAPI.js
@@ -5,19 +5,20 @@ import {firebaseAPIkey, newsAPIkey, iexCloudAPIkey} from './apikeys.js'
 
 $(()=>{
     
-    
     // News API search
-
-    var $searchSubmit = $('#searchSubmit')
-    $searchSubmit.click(function() {
-        var $searchContent = $('#searchContent')[0].value
+    var $searchSubmit = $('#nameList')
+    $searchSubmit.click(function(e) {
+        // console.log(e.target.innerHTML)
+        var $searchContent = e.target.innerHTML
         var url = `https://newsapi.org/v2/everything?q=${$searchContent}&sortBy=relevancy&apiKey=${newsAPIkey}`
         $.get(url)
         .done(function (response) {
             // console.log(response.articles)
-            for (let i = 0; i < response.articles.length; i++) {
+            for (let i = 0; i < 3; i++) {
                 console.log(response.articles[i].title)
             }
         })
     })
+
+
 })
