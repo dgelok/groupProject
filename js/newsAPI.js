621c6110f7ffdd6d1b48a6249f0f596a44d71bb4
Dan Gelok <dgelok@gmail.com>
2020-07-11 16:38:51 -0500

    backing up

--- js/newsAPI.js
+++ js/newsAPI.js
@@ -6,20 +6,20 @@ import {firebaseAPIkey, newsAPIkey, iexCloudAPIkey} from './apikeys.js'
 $(()=>{
     
     // News API search
-    var $searchSubmit = $('#nameList')
-    $searchSubmit.click(function(e) {
-        // console.log(e.target.innerHTML)
-        var $searchContent = e.target.innerHTML
-        var url = `https://newsapi.org/v2/everything?q=${$searchContent}&sortBy=relevancy&apiKey=${newsAPIkey}`
-        $.get(url)
-        .done(function (response) {
-            // console.log(response.articles)
-            let $tbody = $('#tbody')
-            for (let i = 0; i < 3; i++) {
-                console.log(response.articles[i].title)
-            }
-        })
-    })
+    // var $searchSubmit = $('#nameList')
+    // $searchSubmit.click(function(e) {
+    //     // console.log(e.target.innerHTML)
+    //     var $searchContent = e.target.innerHTML
+    //     var url = `https://newsapi.org/v2/everything?q=${$searchContent}&sortBy=relevancy&apiKey=${newsAPIkey}`
+    //     $.get(url)
+    //     .done(function (response) {
+    //         // console.log(response.articles)
+    //         let $tbody = $('#tbody')
+    //         for (let i = 0; i < 3; i++) {
+    //             console.log(response.articles[i].title)
+    //         }
+    //     })
+    // })
 
 
 })
